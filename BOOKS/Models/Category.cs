72e00a732830e4d737d0e88d6ebfb6a5712d8707
IdeaXhafa <ix51808@ubt-uni.net>
2022-12-22 22:47:15 +0100

    trying to show statistic charts for books

--- BOOKS/Models/Category.cs
+++ BOOKS/Models/Category.cs
@@ -9,6 +9,6 @@ namespace BOOKS.Models
     public class Category
     {
         public int id { get; set; }
-public string CategoryName { get; set; }
+        public string CategoryName { get; set; }
     }
 }
