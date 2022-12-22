a7f8ef32371e5a37bc83c8575faf5d52ea8b5794
OrgesMat <orges.69matoshi@gmail.com>
2022-12-22 19:45:06 +0100

    Added AudioBooks and Category models

---
+++ BOOKS/Models/Category.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOOKS.Models
+{    
+    public class Category
+    {
+        public int id { get; set; }
+public string CategoryName { get; set; }
+    }
+}
